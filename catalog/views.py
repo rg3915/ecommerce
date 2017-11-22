c8c1c99ce1e4099cd47e94c8161efee9faa8cb94
rg3915 <rg3915@yahoo.com.br>
2017-11-22 18:39:17 -0200

    Movendo template de produtos para Catalog

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
+
+def product_list(request):
+
+    return render(request, 'catalog/product_list.html')
+
+
+def product(request):
+
+    return render(request, 'catalog/product.html')
--- djangoecommerce/urls.py
+++ djangoecommerce/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import url
 from django.contrib import admin
-from core import views
+from core import views as core
+from catalog import views as cat
 
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^contato/$', views.contact, name='contact'),
-    url(r'^produto/$', views.product, name='product'),
-    url(r'^produtos/$', views.product_list, name='product_list'),
+    url(r'^$', core.index, name='index'),
+    url(r'^contato/$', core.contact, name='contact'),
+    url(r'^produto/$', cat.product, name='product'),
+    url(r'^produtos/$', cat.product_list, name='product_list'),
     url(r'^admin/', admin.site.urls),
 ]
