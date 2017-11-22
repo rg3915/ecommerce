d6cd177ecca8f8ec45bb40173f31b95ef3299866
rg3915 <rg3915@yahoo.com.br>
2017-11-22 17:43:07 -0200

    Customizando Admin

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'created')
+    search_fields = ('name', 'slug')
+    list_filter = ('created', 'modified')
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'category', 'created')
+    search_fields = ('name', 'slug', 'category__name')
+    list_filter = ('created', 'modified', 'category')
--- catalog/models.py
+++ catalog/models.py
@@ -12,6 +12,9 @@ class Category(models.Model):
         verbose_name = 'categoria'
         verbose_name_plural = 'categorias'
 
+    def __str__(self):
+        return self.name
+
 
 class Product(models.Model):
     name = models.CharField('nome', max_length=100)
@@ -26,3 +29,6 @@ class Product(models.Model):
         ordering = ['name']
         verbose_name = 'produto'
         verbose_name_plural = 'produtos'
+
+    def __str__(self):
+        return self.name
