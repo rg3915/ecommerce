8be7ceb15302f5c6ac5ed7e8d76e5de1647e5a8e
rg3915 <rg3915@yahoo.com.br>
2017-11-22 19:28:10 -0200

    get_absolute_url

--- catalog/models.py
+++ catalog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.urlresolvers import reverse
 
 
 class Category(models.Model):
@@ -14,6 +15,9 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('catalog:category', kwargs={'slug': self.slug})
 
 
 class Product(models.Model):
