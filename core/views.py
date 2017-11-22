52bb0dbc0b887bd040dac2b7caf7607dcf2abf73
rg3915 <rg3915@yahoo.com.br>
2017-11-22 16:03:24 -0200

    Template index.html

--- core/views.py
+++ core/views.py
@@ -4,3 +4,13 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse('Hello World')
+
+
+def index(request):
+    texts = ['Lorem ipsum dolor sit amet', 'consectetur adipisicing elit',
+             'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua']
+    context = {
+        'title': 'django e-commerce',
+        'texts': texts
+    }
+    return render(request, 'index.html', context)
