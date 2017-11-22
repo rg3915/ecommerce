0a67bb779d3daa6bb410b06214d5d76b9f140f60
rg3915 <rg3915@yahoo.com.br>
2017-11-22 16:41:52 -0200

    Herança de Templates

--- core/views.py
+++ core/views.py
@@ -14,3 +14,15 @@ def index(request):
         'texts': texts
     }
     return render(request, 'index.html', context)
+
+
+def contact(request):
+    return render(request, 'contact.html')
+
+
+def product_list(request):
+    return render(request, 'product_list.html')
+
+
+def product(request):
+    return render(request, 'product.html')
