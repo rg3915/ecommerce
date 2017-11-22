4a70749d83a45760a363404c32064701558e6fb1
rg3915 <rg3915@yahoo.com.br>
2017-11-22 18:58:48 -0200

    Listando os produtos

--- catalog/views.py
+++ catalog/views.py
@@ -3,10 +3,10 @@ from .models import Product
 
 
 def product_list(request):
-
-    return render(request, 'catalog/product_list.html')
+    products = Product.objects.all()
+    context = {'products': products}
+    return render(request, 'catalog/product_list.html', context)
 
 
 def product(request):
-
     return render(request, 'catalog/product.html')
