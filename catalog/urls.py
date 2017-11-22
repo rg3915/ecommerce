1bc68368fe2c6bf4ea73e75d1b1c0210706a6db7
rg3915 <rg3915@yahoo.com.br>
2017-11-22 19:28:41 -0200

    Link para cada categoria

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,6 +3,7 @@ from catalog import views as cat
 
 
 urlpatterns = [
-    # url(r'^produto/$', cat.product, name='product'),
     url(r'^$', cat.product_list, name='product_list'),
+    url(r'^(?P<slug>[\w_-]+)/$', cat.category, name='category'),
+    # url(r'^produto/$', cat.product, name='product'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Product
+from .models import Category, Product
 
 
 def product_list(request):
@@ -8,5 +8,11 @@ def product_list(request):
     return render(request, 'catalog/product_list.html', context)
 
 
-def product(request):
-    return render(request, 'catalog/product.html')
+def category(request, slug):
+    category = Category.objects.get(slug=slug)
+    products = Product.objects.filter(category=category)
+    context = {
+        'current_category': category,
+        'product_list': products,
+    }
+    return render(request, 'catalog/category.html', context)
