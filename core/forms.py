edb60d73a01c8484ee59aeeeeee95cc877eb4487
rg3915 <rg3915@yahoo.com.br>
2017-12-04 22:43:16 -0200

    ContactForm

---
+++ core/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+
+
+class ContactForm(forms.Form):
+    name = forms.CharField(label='nome')
+    email = forms.EmailField(label='e-mail')
+    message = forms.CharField(label='mensagem', widget=forms.Textarea)
