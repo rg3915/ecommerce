4911f819f68232b3bb5832c15863a5ade29b2b56
rg3915 <rg3915@yahoo.com.br>
2017-12-29 11:14:43 -0200

    Tratando parametros adicionais da url.

--- core/templatetags/pagination.py
+++ core/templatetags/pagination.py
@@ -9,4 +9,12 @@ def pagination(request, paginator, page_obj):
     context['paginator'] = paginator
     context['request'] = request
     context['page_obj'] = page_obj
+    getvars = request.GET.copy()
+    # Tratando parametros adicionais da url.
+    if 'page' in getvars:
+        del getvars['page']
+    if len(getvars) > 0:
+        context['getvars'] = '&{0}'.format(getvars.urlencode())
+    else:
+        context['getvars'] = ''
     return context
