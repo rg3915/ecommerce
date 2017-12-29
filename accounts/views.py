c0d77279844fab8605c3771cc02d3fc417a7d916
rg3915 <rg3915@yahoo.com.br>
2017-12-29 15:33:29 -0200

    Accounts IndexView

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,16 @@
-from django.shortcuts import render
-from django.views.generic import CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.urlresolvers import reverse_lazy
+from django.shortcuts import render
+from django.views.generic import CreateView, TemplateView
 
 from .models import User
 from .forms import UserAdminCreationForm
+
+
+class IndexView(LoginRequiredMixin, TemplateView):
+    template_name = 'accounts/index.html'
+
+index = IndexView.as_view()
 
 
 class RegisterView(CreateView):
