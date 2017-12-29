9f66d859da3359d687c3a8166bed92679a294f35
rg3915 <rg3915@yahoo.com.br>
2017-12-29 13:30:02 -0200

    RegisterViewTestCase

--- core/tests/test_views.py
+++ core/tests/test_views.py
@@ -1,11 +1,9 @@
-from django.test import TestCase, Client
-from django.core.urlresolvers import reverse
-from django.core import mail
-from django.contrib.auth import get_user_model
 from django.conf import settings
-
-# from model_mommy import mommy
-
+from django.contrib.auth import get_user_model
+from django.core import mail
+from django.core.urlresolvers import reverse
+from django.test import TestCase, Client
+from model_mommy import mommy
 
 User = get_user_model()
 
@@ -59,3 +57,57 @@ class ContactViewTestCase(TestCase):
         self.assertEquals(len(mail.outbox), 1)
         self.assertEquals(mail.outbox[0].subject,
                           'Contato do Django E-Commerce')
+
+
+class LoginViewTestCase(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.url = reverse('login')
+        self.user = mommy.prepare(settings.AUTH_USER_MODEL)
+        self.user.set_password('demodemo')
+        self.user.save()
+
+    def tearDown(self):
+        self.user.delete()
+
+    def test_status_code(self):
+        response = self.client.get(self.url)
+        self.assertEquals(response.status_code, 200)
+
+    def test_template_used(self):
+        response = self.client.get(self.url)
+        self.assertTemplateUsed(response, 'login.html')
+
+    def test_login_ok(self):
+        data = {'username': self.user.username, 'password': 'demodemo'}
+        response = self.client.post(self.url, data)
+        redirect_url = reverse(settings.LOGIN_REDIRECT_URL)
+        self.assertRedirects(response, redirect_url)
+        self.assertTrue(response.wsgi_request.user.is_authenticated)
+
+    def test_login_error(self):
+        data = {'username': self.user.username, 'password': '1234'}
+        response = self.client.post(self.url, data)
+        error_msg = ('Por favor, entre com um usuário  e senha corretos.'
+                     ' Note que ambos os campos diferenciam maiúsculas e minúsculas.')
+        self.assertFormError(response, 'form', None, error_msg)
+
+
+class RegisterViewTestCase(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.register_url = reverse('register')
+
+    def test_register_ok(self):
+        data = {
+            'username': 'regis',
+            'password1': 'demodemo',
+            'password2': 'demodemo'
+        }
+        response = self.client.post(self.register_url, data)
+        index_url = reverse('index')
+        self.assertRedirects(response, index_url)
+        # Verifica se existe o usuário.
+        self.assertEquals(User.objects.count(), 1)
