ea7e366873e399cb773d6f973a0379e589363f83
rg3915 <rg3915@yahoo.com.br>
2018-01-16 19:46:15 -0200

    UpdatePasswordView

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,8 @@
+from django.contrib.auth.forms import PasswordChangeForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.urlresolvers import reverse_lazy
 from django.shortcuts import render
-from django.views.generic import CreateView, TemplateView, UpdateView
+from django.views.generic import CreateView, TemplateView, UpdateView, FormView
 
 from .models import User
 from .forms import UserAdminCreationForm
@@ -34,3 +35,22 @@ class UpdateUserView(LoginRequiredMixin, UpdateView):
 
 
 update_user = UpdateUserView.as_view()
+
+
+class UpdatePasswordView(LoginRequiredMixin, FormView):
+
+    template_name = 'accounts/update_password.html'
+    success_url = reverse_lazy('accounts:index')
+    form_class = PasswordChangeForm
+
+    def get_form_kwargs(self):
+        kwargs = super(UpdatePasswordView, self).get_form_kwargs()
+        kwargs['user'] = self.request.user
+        return kwargs
+
+    def form_valid(self, form):
+        form.save()
+        return super(UpdatePasswordView, self).form_valid(form)
+
+
+update_password = UpdatePasswordView.as_view()
