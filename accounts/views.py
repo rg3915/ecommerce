3c54ca08ba783f64af24eaa13d423b18cc14d8b0
rg3915 <rg3915@yahoo.com.br>
2018-01-16 19:05:44 -0200

    UpdateUserView

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.urlresolvers import reverse_lazy
 from django.shortcuts import render
-from django.views.generic import CreateView, TemplateView
+from django.views.generic import CreateView, TemplateView, UpdateView
 
 from .models import User
 from .forms import UserAdminCreationForm
@@ -21,3 +21,16 @@ class RegisterView(CreateView):
 
 
 register = RegisterView.as_view()
+
+
+class UpdateUserView(LoginRequiredMixin, UpdateView):
+    model = User
+    template_name = 'accounts/update_user.html'
+    fields = ['name', 'email']
+    success_url = reverse_lazy('accounts:index')
+
+    def get_object(self):
+        return self.request.user
+
+
+update_user = UpdateUserView.as_view()
