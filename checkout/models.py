fbfb2288bc54a983b138e0f92f3107b5e93bcdf4
rg3915 <rg3915@yahoo.com.br>
2018-01-17 20:02:35 -0200

    Modelagem do Pedido

--- checkout/models.py
+++ checkout/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 
 class CartItemManager(models.Manager):
@@ -46,3 +47,49 @@ models.signals.post_save.connect(
     sender=CartItem,
     dispatch_uid='post_save_cart_item'
 )
+
+
+class Order(models.Model):
+    STATUS_CHOICES = (
+        (0, 'Aguardando Pagamento'),
+        (1, 'Concluída'),
+        (2, 'Cancelada'),
+    )
+    PAYMENT_OPTION_CHOICES = (
+        ('pagseguro', 'PagSeguro'),
+        ('paypal', 'Paypal'),
+    )
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name='Usuário')
+    status = models.IntegerField(
+        'Situação', choices=STATUS_CHOICES, default=0, blank=True
+    )
+    payment_option = models.CharField(
+        'Opção de Pagamento', choices=PAYMENT_OPTION_CHOICES, max_length=20
+    )
+    created = models.DateTimeField('Criado em', auto_now_add=True)
+    modified = models.DateTimeField('Modificado em', auto_now=True)
+
+    class Meta:
+        verbose_name = 'Pedido'
+        verbose_name_plural = 'Pedidos'
+
+    def __str__(self):
+        return 'Pedido #{}'.format(self.pk)
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(
+        Order,
+        verbose_name='Pedido',
+        related_name='items'
+    )
+    product = models.ForeignKey('catalog.Product', verbose_name='Produto')
+    quantity = models.PositiveIntegerField('Quantidade', default=1)
+    price = models.DecimalField('Preço', decimal_places=2, max_digits=5)
+
+    class Meta:
+        verbose_name = 'Item do pedido'
+        verbose_name_plural = 'Itens dos pedidos'
+
+    def __str__(self):
+        return '[{}] {}'.format(self.order, self.product)
